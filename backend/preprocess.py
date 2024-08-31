5f12b433300ee7ccb2c5e591782c561cc3dfce8a
Unmani Shinde <mailto.unmani@gmail.com>
2024-08-31 19:18:13 +0530

    started backend.

---
+++ backend/preprocess.py
@@ -0,0 +1,19 @@
+import requests # for making HTTP requests
+
+import zipfile # fro working with zip files
+
+import os # for interacting with OS
+
+import pandas as pd
+
+import ftplib # for FTP operations
+
+from getpass import getpass #to securely input a password
+
+import xarray as xr # for working with multi-dimentional arrays and datasets
+
+import numpy as np
+
+from datetime import datetime, timedelta 
+
+import matplotlib.pyplot as plt
